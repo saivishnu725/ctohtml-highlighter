d12ddf10d109da8b6f85a3aa269c915eef1ded72
Sai Vishnu <saivishnu725@gmail.com>
2023-04-06 17:21:47 +0530

    isKeyword

--- cparser.c
+++ cparser.c
@@ -2,3 +2,27 @@
 #include <ctype.h>
 #include <string.h>
 
+enum TokenType {
+    IDENTIFIER, INTEGER, FLOAT, STRING, CHAR, KEYWORD, OPERATOR, PUNCTUATOR
+};
+
+struct Token {
+    enum TokenType type;
+    char value[100];
+};
+
+int isKeyword(char *str) {
+    char keywords[32][10] = {"auto", "break", "case", "char", "const", "continue", "default",
+                             "do", "double", "else", "enum", "extern", "float", "for", "goto",
+                             "if", "int", "long", "register", "return", "short", "signed",
+                             "sizeof", "static", "struct", "switch", "typedef", "union",
+                             "unsigned", "void", "volatile", "while"
+                            };
+    int i;
+    for (i = 0; i < 32; ++i) {
+        if (strcmp(keywords[i], str) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
--- ctohtml.c
+++ ctohtml.c
@@ -3,6 +3,7 @@
 
 int main()
 {
-    
+    // printf("12 is %d \n", isKeyword("12"));          0
+    // printf("char is %d \n", isKeyword("char"));      1
     return 0;
 }
